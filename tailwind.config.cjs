cef7489eb113f43f783f49fa17c5a85c252755c5
anonymous-atharvv <07anonymous.ananta@gmail.com>
2025-11-14 12:07:43 +0500

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html","./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        jifblue: {
+          50: '#f7fbfd',
+          100: '#eef6fb',
+          500: '#1f4b7d',
+          700: '#0e2a4a'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
